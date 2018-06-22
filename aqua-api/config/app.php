1420138d2cc6aaa7a67af71b4e20494b047fdb75
qiaojun0312 <949042858@qq.com>
2018-06-22 18:52:18 +0800

    update

--- aqua-api/config/app.php
+++ aqua-api/config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+   * easywechat
+   */
+        Overtrue\LaravelWechat\ServiceProvider::class,
+
     ],
 
     /*
