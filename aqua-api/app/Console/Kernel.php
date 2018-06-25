d11327828a85f355a956f4ed4d886a20c5004635
qiaojun0312 <949042858@qq.com>
2018-06-25 19:17:26 +0800

    update

--- aqua-api/app/Console/Kernel.php
+++ aqua-api/app/Console/Kernel.php
@@ -2,8 +2,13 @@
 
 namespace App\Console;
 
+
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use App\Models\UserWinner;
+use App\Models\UserInfo;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +18,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+       // \App\Console\Commands\Inspire::class,
     ];
 
     /**
@@ -24,8 +29,93 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+
+
+            //当前日期
+            $created_at= date("Y-m-d");
+            $users_winner = DB::select("select *
+                            from user_winners as uswinner
+                            where uswinner.created_at like '".$created_at."%'");
+            $totalcount =count($users_winner);
+
+            //如果数据库有当天记录就说明生成过了，不执行生成中奖操作
+            if($totalcount <= 0)
+            {
+                DB::beginTransaction();
+                try {
+
+                    //昨天分数排行榜
+                    $users_score = DB::select("select uscore.id,fromuser.openid
+                                    from user_scores as uscore
+                                    left join user_infos as fromuser on uscore.openid =fromuser.openid
+                                    where uscore.created_at like '" . $created_at . "%' and fromuser.isflag=0  order by uscore.score desc ");
+
+                    $arrayTop5 = array();//前5名
+                    $arrayOther = array();//除去前5剩下的
+                    $i = 0;
+                    foreach ($users_score as $scores) {
+                        if ($i < 5) //获取前5名
+                        {
+                            array_push($arrayTop5, $scores->openid);
+                        } else //获取除了前5名之外的
+                        {
+                            array_push($arrayOther, $scores->openid);
+                        }
+                        $i++;
+                    }
+
+                    //新增排名前5的中奖用户
+                    foreach ($arrayTop5 as $arrayTop5item) {
+                        //新增
+                        $UserWinner = new UserWinner();
+                        $UserWinner->openid = $arrayTop5item;
+                        $UserWinner->zjtype = 'top5';
+
+                        $UserWinner->save();
+
+                        //修改用户是否中奖标示
+                        $user = UserInfo::where('openid', $arrayTop5item)->get();
+                        if ($user->count()) {
+                            $user[0]->isflag = 1;
+                            $user[0]->save();
+                        }
+                    }
+                    if ($i > 4) {
+                        //从剩下的数组中随机产生5个中奖用户
+                        $arrayAfter5 = array_rand($arrayOther, 5);
+
+                        //新增随机产生的5名用户
+                        foreach ($arrayAfter5 as $arrayAfter5item) {
+                            //新增
+                            $UserWinner = new UserWinner();
+                            $UserWinner->openid = $arrayOther[$arrayAfter5item];
+                            $UserWinner->zjtype = 'random';
+
+                            $UserWinner->save();
+
+                            //修改用户是否中奖标示
+                            $user = UserInfo::where('openid', $UserWinner->openid)->get();
+                            if ($user->count()) {
+                                $user[0]->isflag = 1;
+                                $user[0]->save();
+                            }
+                        }
+                    }
+
+                    DB::commit();
+                    Log::info("执行成功！");
+                } catch (Exception $ex) {
+                    DB::rollback();
+                    throw $ex;
+                    Log::info("执行失败！");
+                }
+            }else
+            {
+                Log::info("当天已经执行过了！");
+            }
+
+        })->dailyAt('23:55');
     }
 
     /**
